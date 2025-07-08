6ced8c03ff8f653b0c20b8ee21845a86b81f57b8
UrviArora218 <urviarora0810@gmail.com>
2025-07-08 11:12:15 +0530

    Create test cases

--- math.js
+++ math.js
@@ -1,4 +1,11 @@
 function add(a,b){
     return a+b;
 }
-module.exports=add;
+
+
+function divide(a, b) {
+  if (b === 0) throw new Error("Cannot divide by zero");
+  return a / b;
+}
+
+module.exports = {add,divide};
--- math.test.js
+++ math.test.js
@@ -1,11 +1,24 @@
 
 
 
-const add=require('./math');
+const {add,divide}=require('./math');
  test('adds 2+3 to equal 5',()=>{
     const actual =add(2,3);
     const expected=5;
     expect(actual).toBe(expected);
  
  });
- console.log('All tests passed!');
+ 
+
+ 
+
+
+test('divides 10 by 2 to equal 5', () => {
+  const actual = divide(10, 2);
+  const expected = 5;
+  expect(actual).toBe(expected);
+});
+
+test('throws error when dividing by zero', () => {
+  expect(() => divide(10, 0)).toThrow("Cannot divide by zero");
+});
